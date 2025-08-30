65712b94c8cb75639274c8665dede883d5f9d984
Snider <snider@lt.hn>
2025-08-30 15:01:04 +0100

    Refactor main.cpp for code cleanup and modernization
    Updated copyright notice, removed unused includes and commented code, replaced deprecated WinAPI calls with modern equivalents, and improved namespace usage. Also refactored application execution and initialization for clarity and maintainability.

--- src/gui/qt-daemon/main.cpp
+++ src/gui/qt-daemon/main.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2017-2025 Lethean VPN
 // Copyright (c) 2014-2018 Zano Project
 // Copyright (c) 2014-2018 The Louisdor Project
 // Copyright (c) 2012-2013 The Boolberry developers
@@ -6,29 +7,24 @@
 
 #include <QApplication>
 #include "application/mainwindow.h"
-#include "qdebug.h"
-#include <thread>
-#include <math.h>
-//#include "qtlogger.h"
+// #include "qdebug.h"
+#include <iostream>
+// ReSharper disable once CppUnusedIncludeDirective
 #include "include_base_utils.h"
 #include "currency_core/currency_config.h"
 #ifdef Q_OS_DARWIN
 #include "application/urleventfilter.h"
 #endif
 
+namespace
+{
+  using string_encoding::wstring_to_utf8;
+  using string_tools::set_module_name_and_folder;
+}
+
 int main(int argc, char *argv[])
 {
-//#if defined(ARCH_CPU_X86_64) && _MSC_VER <= 1800
-       // VS2013's CRT only checks the existence of FMA3 instructions, not the
-       // enabled-ness of them at the OS level (this is fixed in VS2015). We force
-       // off usage of FMA3 instructions in the CRT to avoid using that path and
-       // hitting illegal instructions when running on CPUs that support FMA3, but
-       // OSs that don't. Because we use the static library CRT we have to call
-       // this function once in each DLL.
-       // See http://crbug.com/436603.
-//       _set_FMA3_enable(0);
-//#endif  // ARCH_CPU_X86_64 && _MSC_VER <= 1800
-  
+
   if(argc > 1)
     std::cout << argv[1] << std::endl;
 
@@ -37,28 +33,20 @@ int main(int argc, char *argv[])
   _set_FMA3_enable(0);
   #endif
   //mutex to let InnoSetup know about running instance
-  ::CreateMutex(NULL, FALSE, boost::winapi::LPCWSTR_(CURRENCY_NAME_BASE "_instance"));
-  //::CreateMutex(NULL, FALSE, "Global\\" CURRENCY_NAME_BASE "_instance");
+  CreateMutexA(nullptr, FALSE, CURRENCY_NAME_BASE "_instance");
 #endif
 
 
 #ifdef WIN32
   WCHAR sz_file_name[MAX_PATH + 1] = L"";
-  ::GetModuleFileNameW(NULL, sz_file_name, MAX_PATH + 1);
-  std::string path_to_process_utf8 = epee::string_encoding::wstring_to_utf8(sz_file_name);
+  GetModuleFileNameW(nullptr, sz_file_name, MAX_PATH + 1);
+  std::string path_to_process_utf8 = wstring_to_utf8(sz_file_name);
 #else
   std::string path_to_process_utf8 = argv[0];
 #endif
 
   TRY_ENTRY();
-  epee::string_tools::set_module_name_and_folder(path_to_process_utf8);
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#ifdef _MSC_VER
-#if _MSC_VER >= 1910
-  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps); //HiDPI pixmaps
-  //qputenv("QT_SCALE_FACTOR", "0.75");
-#endif
-#endif
+  set_module_name_and_folder(path_to_process_utf8);
 
   log_space::get_set_log_detalisation_level(true, LOG_LEVEL_0);
   log_space::get_set_need_thread_id(true, true);
@@ -82,7 +70,7 @@ int main(int argc, char *argv[])
   viewer.setWindowTitle(CURRENCY_NAME_BASE);
   viewer.show_inital();
 
-  int res = app.exec();
+  int res = QApplication::exec();
   LOG_PRINT_L0("Process exit with code: " << res);
   return res;
   CATCH_ENTRY2(0);
