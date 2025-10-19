2289d4e1817c05cb6f167fa783529c715ae8d398
Snider <snider@lt.hn>
2025-10-19 15:13:59 +0100

    Add Angular SDK client generated from OpenAPI
    Introduces a new Angular client SDK for the Lethean Blockchain API, generated via OpenAPI Generator. Includes all necessary Angular service, model, configuration, and packaging files, and updates the Makefile to support the 'angular' target for SDK generation.

---
+++ utils/sdk/client/angular/api/api.ts
@@ -0,0 +1,5 @@
+export * from './block.service';
+import { BlockService } from './block.service';
+export * from './info.service';
+import { InfoService } from './info.service';
+export const APIS = [BlockService, InfoService];
